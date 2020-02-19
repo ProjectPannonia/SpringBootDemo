fb60fefd8c85260bc0f4b9d08d4142ae7271e9e6
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2020-02-19 22:09:15 +0100

    Main method(Springbootapplication) implemented

--- src/main/java/springbootdemo/EmployeeController.java
+++ src/main/java/springbootdemo/EmployeeController.java
@@ -1,5 +1,34 @@
 package springbootdemo;
 
-public class EmployeeController {
+import java.net.URI;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping(path = "/employees")
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeDAO employeeDao;
+	
+	@GetMapping(path = "/",produces = "application/json")
+	public Employees getEmployees() {
+		return employeeDao.getAllEmployees();
+	}
+	
+	public ResponseEntity<Object> addEmployee(@RequestBody Employee employee){
+		Integer id = employeeDao.getAllEmployees().getEmployeeList().size()+1;
+		employee.setId(id);
+		employeeDao.addEmployee(employee);
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(employee.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
+	}
 }
